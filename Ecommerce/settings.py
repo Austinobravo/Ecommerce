d265205a9d17fe54ccf81cafa3aa722f43a8a762
Austinobravo <Austinobravo@gmail.com>
2022-09-06 04:44:52 +0100

    url

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -77,14 +77,20 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Ecommerce_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'karmakaz210',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Ecommerce_db',
-        'USER': 'postgres',
-        'PASSWORD': 'karmakaz210',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
